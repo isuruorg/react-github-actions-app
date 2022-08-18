c2544a69413c61db84f1c312ad11fb7bec567fa9
IsuruHL <isurugodakandahl@gmail.com>
2022-08-19 00:55:53 +0530

    Initial commit

--- src/utils/reportTypes.js
+++
@@ -1,7 +0,0 @@
-// export const POLYGON_REPORT_TYPES_OPTIONS = { normal: 'Normal', accuracy: 'Accuracy', geoJson: 'Geo Json' };
-
-export const POLYGON_REPORT_TYPES_OPTIONS = [
-  { label: 'Normal', value: 'normal' },
-  { label: 'Accuracy', value: 'accuracy' },
-  { label: 'Geo JSON', value: 'geoJson' }
-];
